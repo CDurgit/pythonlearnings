c9cdd24ac311d1b414b481b89019202bc1774d22
Alex <sanalive144@gmail.com>
2021-08-28 21:51:04 +0300

    1.0 Homework Fix

--- 1.1.py
+++ 1.1.py
@@ -1,2 +1,15 @@
-while True:
-      lobbycomand = input('InLobby ')
+usertime = int(input('Enter time in seconds: '))
+minutes = usertime // 60
+seconds = usertime % 60
+hours = minutes // 60
+minutes -= 60 * hours
+days = hours // 24
+hours -= 24 * days
+weeks = days // 7
+days -= 7 * weeks
+months = weeks // 4
+weeks -= 4 * months
+years = months // 12
+months -= 12 * years
+print(years, 'years', months, 'months', weeks, 'weeks', days, 'days', hours, 'hours', minutes, 'minutes', seconds,
+      'seconds')
